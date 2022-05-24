20f763cee0dd3c1ba61ed60e15f6c58c0c7784ba
lolu1023 <soowkw@naver.com>
2022-05-24 21:57:04 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Header(props) {
+  console.log("props",props)
+  return(
+    <div>
+      <h1>{props.title}</h1>
     </div>
-  );
+  )
 }
+
+function Nav(props) {
+  const lis = []
+  for(let i = 0; i < props.topices.length; i++) {
+    let t = props.topices[i]
+    lis.push(<li>{t.title}</li>)
+  }
+  return(
+    <ol>
+      {lis}
+    </ol>
+  )
+}
+
+function Bottom(props) {
+  return(
+    <div>
+      <h2>{props.title}</h2>
+      <p>{props.p}</p>
+    </div>
+  )
+}
+
+function App() {
+  const topices = [
+    {id: 1, title: 'one'},
+    {id: 2, title: 'two'},
+    {id: 3, title: 'three'}
+  ]
+  return(
+    <div>
+      <Header title="WEB"></Header>
+      <Nav topices={topices}></Nav>
+      <Bottom title="bot" p="zzzzzzzzzz"></Bottom>
+    </div>
+  )
+}
+
 
 export default App;
